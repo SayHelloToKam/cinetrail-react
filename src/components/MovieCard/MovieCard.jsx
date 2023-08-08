9ee2119b406217a417a050c2a19dc0880ee2dca1
SayHelloToKam <sayhello@kamilwatson.com>
2023-08-08 18:27:53 -0400

    completed Popular Card and pagination

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import StarRatings from "react-star-ratings";
+
+export default function MovieCard({
+  movie,
+  height,
+  width,
+  cardStyle,
+  radius,
+  imgSrc,
+  id,
+}) {
+  const movieCardStyle = {
+    backgroundImage: `url(${import.meta.env.VITE_API_BASE_IMAGE_URL}${imgSrc})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    position: "relative",
+    height,
+    width,
+    borderRadius: radius,
+    boxShadow:
+      cardStyle === "popular-card"
+        ? "0px 0px 10px 0px rgba(118, 118, 118, 0.75)"
+        : null,
+  };
+
+  const hyperRef = `/movieDetails/${id}`;
+
+  return (
+    <Link className={cardStyle} to={hyperRef}>
+      <div style={movieCardStyle}>
+        <div className='movie-info-top'>
+          {movie && (
+            <StarRatings
+              rating={movie?.vote_average / 2}
+              starRatedColor='red'
+              starDimension='18px'
+              starSpacing='1px'
+              numberOfStars={5}
+              name='rating'
+            />
+          )}
+        </div>
+        <div className='movie-info-bottom'>
+          <p>{movie?.title}</p>
+          <p>Rating:&nbsp; {Math.round(movie?.vote_average / 2)}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Slider from "../components/Slider/Slider";
+import PopularMovies from "../components/PopularMovies/PopularMovies";
+import "./styles.css";
+import "./movies.css";
 
 function HomePage() {
   return (
     <div>
       <Slider />
+      <PopularMovies />
     </div>
   );
 }
